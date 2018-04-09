befe769d506d183f44f48f1afb6bf1716c00610c
nanofarade <nadia.trabelsi@esprit.tn>
2018-04-09 15:42:49 +0100

    Views and controllers update

--- src/MySoulMate/MainBundle/Entity/Profil.php
+++ src/MySoulMate/MainBundle/Entity/Profil.php
@@ -55,6 +55,86 @@ class Profil
      */
     private $preference;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param string $photo
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return \Caracteristique
+     */
+    public function getCaracteristique()
+    {
+        return $this->caracteristique;
+    }
+
+    /**
+     * @param \Caracteristique $caracteristique
+     */
+    public function setCaracteristique($caracteristique)
+    {
+        $this->caracteristique = $caracteristique;
+    }
+
+    /**
+     * @return \Caracteristique
+     */
+    public function getPreference()
+    {
+        return $this->preference;
+    }
+
+    /**
+     * @param \Caracteristique $preference
+     */
+    public function setPreference($preference)
+    {
+        $this->preference = $preference;
+    }
+
 
 }
 
