d5f6cc12cef9aab59c8570af47cee402dda3fa08
zhanimm <jileni.zehani@gmail.com>
2018-05-23 06:39:14 +0100

    Merge branch 'master' of https://github.com/RansomGr/MySoulMate-Symphony
    # Conflicts:
    #	app/Resources/views/Layouts/Layout_FO.html.twig
    #	app/config/security.yml

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,6 +13,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
